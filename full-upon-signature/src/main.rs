63dc5b32fa83ac051ca0fe358a56238ae35a6bb9
Shubham <kumar.shubham1312@gmail.com>
2023-04-24 14:02:18 +0530

    [WIP] : full upon signature template still requires paritcipant struct as json

--- full-upon-signature/src/main.rs
+++ full-upon-signature/src/main.rs
@@ -4,30 +4,79 @@ mod lib;
 use lib::org_accordproject_payment_fulluponsignature::*;
 use lib::org_accordproject_signature::*;
 use serde_json::json;
+use serde_json::Value;
 use std::process::Command;
 
-fn get_relationship_as_json(name: &serde_json::Value) -> std::io::Result<serde_json::Value> {
-    let string_json = r#"
-    {
-        "$class": "org.accordproject.signature.ContractSigned",
-        "contractId": "",
-        "$identifier": ""
-    }"#;
+use crate::lib::org_accordproject_obligation::NotificationObligation;
+use crate::lib::org_accordproject_obligation::PaymentObligation;
 
-    let mut sender: serde_json::Value = serde_json::from_str(&string_json)?;
+fn handle_contract() -> String {
+    r#"
+        {
+            "$class": "org.accordproject.payment.fulluponsignature.FullPaymentUponSignatureContract",
+            "buyer": "resource:org.accordproject.party.Party#Dan",
+            "seller": "resource:org.accordproject.party.Party#Steve",
+            "amount": {
+                "$class": "org.accordproject.money.MonetaryAmount",
+                "doubleValue": 0.01,
+                "currencyCode": "USD"
+            },
+            "contractId": "8e844b8d-f26e-4796-b5e1-c33016d6a76b",
+            "$identifier": "8e844b8d-f26e-4796-b5e1-c33016d6a76b"
+        }
+    "#
+    .to_owned()
+}
+
+fn handle_party() -> String {
+    let cmd = Command::new("concerto")
+        .args([
+            "generate",
+            "sample",
+            "--model",
+            "https://models.accordproject.org/accordproject/party.cto",
+            "--concept",
+            "org.accordproject.party.Party",
+        ])
+        .output()
+        .expect("failed to execute process");
+
+    String::from_utf8_lossy(&cmd.stdout).into_owned()
+}
+
+fn get_struct(value: String) -> String {
+    value.split("#").collect::<Vec<&str>>()[0]
+        .split(".")
+        .collect::<Vec<&str>>()
+        .pop()
+        .unwrap_or_default()
+        .to_owned()
+}
+
+fn get_relationship_as_json(name: &serde_json::Value) -> std::io::Result<serde_json::Value> {
+    println!("{:?}", name);
+    let mut sender: serde_json::Value;
+    if let Value::String(value) = name {
+        let string_json = match get_struct(value.clone()).as_str() {
+            "FullPaymentUponSignatureContract" => handle_contract(),
+            "Party" => handle_party(),
+            other => panic!(
+                "{}",
+                format!(
+                    "The {:?} value is not handled by the relationship to json handler",
+                    other
+                )
+            ),
+        };
+        sender = serde_json::from_str(&string_json)?;
+    } else {
+        panic!("The required field is not handled by the relationship handler")
+    };
 
     if let Some(identifier) = sender.get_mut("$identifier") {
         for (i, part) in name.to_string().replace('"', "").split("#").enumerate() {
             if i == 1 {
                 *identifier = part.replace("%20", " ").into()
-            }
-        }
-    }
-
-    if let Some(contract_id) = sender.get_mut("contractId") {
-        for (i, part) in name.to_string().replace('"', "").split("#").enumerate() {
-            if i == 1 {
-                *contract_id = part.replace("%20", " ").into()
             }
         }
     }
@@ -57,9 +106,20 @@ fn main() -> std::io::Result<()> {
             *name = json;
         }
     }
+    if let Some(name) = request.get_mut("promiser") {
+        if let Ok(json) = get_relationship_as_json(&name) {
+            *name = json;
+        }
+    }
 
-    let response: ContractSigned =
-        serde_json::from_str(&request.to_string()).unwrap_or_else(|err| {
+    if let Some(name) = request.get_mut("promisee") {
+        if let Ok(json) = get_relationship_as_json(&name) {
+            *name = json;
+        }
+    }
+
+    let response: NotificationObligation = serde_json::from_str(&request.to_string())
+        .unwrap_or_else(|err| {
             // Ooops!  Didn't work - display an error and exit.
             eprintln!("Error: {}", err);
             std::process::exit(1);
