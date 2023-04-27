7f4f523819521f03c5d46d770eeb43e7a5443b5f
Shubham <kumar.shubham1312@gmail.com>
2023-04-27 21:09:29 +0530

    [WIP] : add the first test case as main function

--- acceptance-of-delivery-clause/src/logic/logic.rs
+++ acceptance-of-delivery-clause/src/logic/logic.rs
@@ -1,42 +1,203 @@
-use std::cmp::Ordering;
+use std::{cmp::Ordering, collections::HashMap, process::Command, str::FromStr};
 
 use lib::{
     org_accordproject_acceptanceofdelivery_0_15_0::{
         AcceptanceOfDeliveryClause, InspectDeliverable, InspectionResponse, InspectionStatus,
     },
+    org_accordproject_organization_0_2_0::Organization,
     org_accordproject_runtime_0_2_0::*,
+    utils::*,
 };
 
 use chrono::offset::Utc;
 use chrono::{naive::Days, DateTime};
+use lambda_http::{Body, Error, IntoResponse, Request, Response};
+use serde_json::{from_str, from_value, Value};
 
-pub fn acceptance_of_delivery(
-    contract: AcceptanceOfDeliveryClause,
-    request: InspectDeliverable,
-) -> Result<InspectionResponse, &'static str> {
-    let received = request.deliverable_received_at;
-    if received <= Utc::now() {
-        return Err("Transaction time is before the deliverable date.");
+const CONTRACT_STR: &str = r#"
+    {
+        "$class": "org.accordproject.acceptanceofdelivery.AcceptanceOfDeliveryClause",
+        "shipper": "resource:org.accordproject.organization.Organization#Party%20A",
+        "receiver": "resource:org.accordproject.organization.Organization#Party%20B",
+        "deliverable": "Widgets",
+        "businessDays": 10,
+        "attachment": "Attachment X",
+        "clauseId": "16ddc768-3847-4fc6-a7e4-9b7d94c020dd",
+        "$identifier": "16ddc768-3847-4fc6-a7e4-9b7d94c020dd"
     }
+"#;
+
+const DELIVERABLE: &str = r#"
+    {
+        "$class": "org.accordproject.acceptanceofdelivery.InspectDeliverable",
+        "deliverableReceivedAt": "2019-01-22 03:24:00Z",
+        "inspectionPassed": true
+    }
+"#;
+
+fn trim(s: &Value) -> String {
+    let Some(s) = s.as_str() else {
+                panic!("cannot convert JSON field value to string")
+            };
+
+    s.trim_matches('\"').to_string()
+}
+
+pub struct AcceptanceOfDeliveryClauseImpl(AcceptanceOfDeliveryClause);
+pub struct OrganizationImpl(Organization);
+pub struct InspectDeliverableImpl(InspectDeliverable);
+pub struct InspectionResponseImpl(InspectionResponse);
+
+impl OrganizationImpl {
+    pub fn new(field_name: &'static str, field_value: String) -> Self {
+        let organization = r#"
+            {
+                "$class": "org.accordproject.organization.Organization",
+                "identifier": "resource1",
+                "$identifier": "resource1"
+            }
+        "#;
+        let organization: HashMap<&str, &str> = from_str(&organization).unwrap();
+
+        let Some(identifier) = field_value
+            .split("#")
+            .last()
+            .and_then(|s| {
+                Some(s.replace("%20", " ").trim_matches('\"').to_string())
+            }) else {
+                panic!("Cannot find the field value")
+            };
+
+        Self(Organization {
+            _class: organization[&"$class"].to_string(),
+            identifier: identifier.clone(),
+            name: Some(field_name.to_string()),
+            description: None,
+            duns: None,
+            place: None,
+            _identifier: identifier,
+        })
+    }
+}
+
+impl AcceptanceOfDeliveryClauseImpl {
+    pub fn new(contract: &'static str) -> Self {
+        let Ok(contract_map) = serde_json::from_str::<HashMap<&'static str, Value>>(&contract) else {
+            panic!("Cannot convert Acceptance of Delivery Clause to HashMap")
+        };
+
+        Self(AcceptanceOfDeliveryClause {
+            _class: trim(&contract_map[&"$class"]),
+            shipper: OrganizationImpl::new("shipper", contract_map[&"shipper"].to_string()).0,
+            receiver: OrganizationImpl::new("receiver", contract_map[&"receiver"].to_string()).0,
+            deliverable: trim(&contract_map[&"deliverable"]),
+            business_days: contract_map[&"businessDays"]
+                .as_u64()
+                .unwrap_or_else(|| panic!("cannot convert business days to u64")),
+            attachment: trim(&contract_map[&"attachment"]),
+            clause_id: trim(&contract_map[&"clauseId"]),
+            _identifier: trim(&contract_map[&"$identifier"]),
+        })
+    }
+}
+
+impl<'a> InspectDeliverableImpl {
+    pub fn new(
+        _class: String,
+        deliverable_received_at: DateTime<Utc>,
+        inspection_passed: bool,
+        _timestamp: DateTime<Utc>,
+    ) -> Self {
+        Self(InspectDeliverable {
+            _class,
+            deliverable_received_at,
+            inspection_passed,
+            _timestamp,
+        })
+    }
+    pub fn from_request(request: Request) -> Result<Self, &'static str> {
+        match request.body().clone() {
+            Body::Empty => Err("The request is empty"),
+            Body::Text(s) => Ok(Self::from_str(s.clone().as_str())),
+            Body::Binary(_) => Err("The request contains binary data which is not handled"),
+        }
+    }
+
+    pub fn from_str(s: &'a str) -> Self {
+        let deliverable = from_str::<HashMap<&'a str, Value>>(s)
+            .unwrap_or_else(|_| panic!("Cannot convert to InspectDeliverable"));
+
+        Self::new(
+            trim(&deliverable[&"$class"]),
+            from_value(deliverable[&"deliverableReceivedAt"].clone())
+                .unwrap_or_else(|_| panic!("Cannot deserialize datetime")),
+            deliverable[&"inspectionPassed"]
+                .as_bool()
+                .unwrap_or_else(|| panic!("cannot get inspection_passed from the json")),
+            Utc::now(),
+        )
+    }
+}
+
+impl<'a> InspectionResponseImpl {
+    pub fn new(
+        status: InspectionStatus,
+        shipper: Organization,
+        receiver: Organization,
+        timestamp: DateTime<Utc>,
+    ) -> InspectionResponseImpl {
+        Self(InspectionResponse {
+            _class: "org.accordproject.acceptanceofdelivery.InspectionResponse".into(),
+            status,
+            shipper,
+            receiver,
+            _timestamp: timestamp, // this is not ideal
+        })
+    }
+
+    pub fn into_response(self) -> Result<Response<Body>, &'a str> {
+        let response = serde_json::to_string(&self.0)
+            .map_err(|_| "Unable convert InspectionResponse struct to string")?;
+
+        Response::builder()
+            .status(200)
+            .header("content-type", "application/json")
+            .body(response.into())
+            .map_err(|_| "Unable to build response using the InspectionResponse string")
+    }
+}
+
+fn now(s: &'static str) -> DateTime<Utc> {
+    DateTime::from_str(s).unwrap_or_else(|_| panic!("Cannot convert string to UTC datetime format"))
+}
+
+pub async fn handler(request: Request) -> Result<Response<Body>, &'static str> {
+    let now = now("2019-01-31T16:34:00-05:00");
+    let contract = AcceptanceOfDeliveryClauseImpl::new(CONTRACT_STR).0;
+    let request = InspectDeliverableImpl::from_request(request)?.0;
+    let received = request.deliverable_received_at;
+    if now < received {
+        return Err("Transaction time is before the deliverable date.");
+    };
 
     let status: InspectionStatus = {
         match received.checked_add_days(Days::new(contract.business_days)) {
-            None => {
-                if request.inspection_passed {
-                    InspectionStatus::PASSED_TESTING
+            None => Err("This is not a valid date"),
+            Some(date) => Ok({
+                if now < date {
+                    if request.inspection_passed {
+                        InspectionStatus::PASSED_TESTING
+                    } else {
+                        InspectionStatus::FAILED_TESTING
+                    }
                 } else {
-                    InspectionStatus::FAILED_TESTING
+                    InspectionStatus::OUTSIDE_INSPECTION_PERIOD
                 }
-            }
-            Some(_) => InspectionStatus::OUTSIDE_INSPECTION_PERIOD,
+            }),
         }
-    };
+    }?;
 
-    return Ok(InspectionResponse {
-        _class: String::new(), // this is not ideal
-        status,
-        shipper: contract.shipper,
-        receiver: contract.receiver,
-        _timestamp: Utc::now(), // this is not ideal
-    });
+    let response = InspectionResponseImpl::new(status, contract.shipper, contract.receiver, now);
+
+    response.into_response()
 }
--- acceptance-of-delivery-clause/src/main.rs
+++ acceptance-of-delivery-clause/src/main.rs
@@ -1,87 +1,17 @@
-use std::fs::File;
-use std::io::Read;
-use lib::org_accordproject_acceptanceofdelivery_0_15_0::*;
-use std::process::Command;
-use lambda_http::{run, service_fn, Body, Error, Request, Response};
+mod logic;
 
-fn get_relationship_as_json(name:&serde_json::Value) -> std::io::Result<serde_json::Value>  {
-    let cmd = Command::new("concerto")
-    .args(["generate", "sample","--model","https://models.accordproject.org/organization.cto","--concept","org.accordproject.organization.Organization"])
-    .output()
-    .expect("failed to execute process");
-
-
-    let string_json = String::from_utf8_lossy(&cmd.stdout);
-
-    let mut sender:serde_json::Value = serde_json::from_str(&string_json)?;
-
-    if let Some(identifier) = sender.get_mut("$identifier") {
-
-    for (i, part) in name.to_string().replace('"',"").split("#").enumerate() {
-            if i == 1 {
-                *identifier = part.replace("%20", " ").into()
-            }
-        }
-    }
-
-    return Ok(sender);
-
-}
-
-async fn function_handler(_: Request) -> Result<Response<Body>, Error> {
-    // Pull the json request object from file.
-    let mut file = File::open("./acceptance-of-delivery-clause/model/request.json")?;
-    let mut request_json = String::new();
-    file.read_to_string(&mut request_json)?;
-
-    // Deserialise MyRequest
-    let mut request: serde_json::Value = serde_json::from_str(&request_json).unwrap_or_else(|err| {
-        // Ooops!  Didn't work - display an error and exit.
-        eprintln!("Error: {}", err);
-        std::process::exit(1);
-    });
-
-    if let Some(name) = request.get_mut("shipper") {
-        if let Ok(json) = get_relationship_as_json(&name) {
-           *name = json;
-
-        }
-    }
-
-    if let Some(name) = request.get_mut("receiver") {
-        if let Ok(json) = get_relationship_as_json(&name) {
-            *name = json;
-         }
-    }
-
-    let response: AcceptanceOfDeliveryClause = serde_json::from_str(&request.to_string()).unwrap_or_else(|err| {
-        // Ooops!  Didn't work - display an error and exit.
-        eprintln!("Error: {}", err);
-        std::process::exit(1);
-    });
-
-    let resp_as_string = serde_json::to_string(&response).unwrap();
-    
-
-    let resp = Response::builder()
-        .status(200)
-        .header("content-type",  "application/json")
-        .body(resp_as_string.into())
-        .map_err(Box::new)?;
-
-    Ok(resp)
-}
-
+use crate::logic::logic::*;
+use lambda_http::{run, service_fn, Error};
 
 #[tokio::main]
 async fn main() -> Result<(), Error> {
     tracing_subscriber::fmt()
-    .with_max_level(tracing::Level::INFO)
-    .with_target(false)
-    .without_time()
-    .init();
-    
-    run(service_fn(function_handler)).await
+        .with_max_level(tracing::Level::INFO)
+        .with_target(false)
+        .without_time()
+        .init();
+
+    run(service_fn(handler)).await
 }
 
 #[cfg(test)]
